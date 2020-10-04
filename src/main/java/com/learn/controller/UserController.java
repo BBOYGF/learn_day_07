51871e808206de1cb56a498275b35a3cf7303e61
guofan_global <66430988@qq.com>
2020-10-05 02:59:47 +0800

    上传文件  MVC方法

--- src/main/java/com/learn/controller/UserController.java
+++ src/main/java/com/learn/controller/UserController.java
@@ -6,11 +6,14 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/user")
@@ -35,6 +38,7 @@ public class UserController {
         //遍历
         for(FileItem item:list)
         {
+            System.out.println("item:"+item.toString()+"\r\n");
             //判断当前item是否是上传文件项
             if(item.isFormField())
             {
@@ -51,5 +55,25 @@ public class UserController {
         }
         return "success";
     }
-
+    @RequestMapping("/fileupload2")
+    public String fileuoload2(HttpServletRequest request, MultipartFile upload) throws IOException {
+        System.out.println("mvc文件上传！");
+        System.out.println("文件上...");
+        //使用upload组件实现上传
+        //上传位置
+        String path = request.getSession().getServletContext().getRealPath("/uploads/");
+        File file=new File(path);
+        if(!file.exists())
+        {
+            file.mkdir();
+        }
+        //获取文件名称
+        String filename = upload.getOriginalFilename();
+        //设置唯一值
+        String uuid = UUID.randomUUID().toString().replace("_", "");
+        filename=uuid+"_"+filename;
+        //上传文件
+        upload.transferTo(new File(path,filename));
+        return "success";
+    }
 }
--- target/learn_day_07/index.jsp
+++ target/learn_day_07/index.jsp
@@ -6,18 +6,27 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java"  isELIgnored="false" %>
-<html>
+<hr>
 <head>
     <title>Title</title>
 </head>
 <br>
 <h3 >文件上传</h3>
 </br>
-
+<h3>传统方式</h3>
+</br>
 <form action="user/fileupload1" method="post" enctype="multipart/form-data">
     选择文件：<input type="file" name="upload"/>
     <input type="submit" value="上传"/>
     </br>
 </form>
+<h3>MVC方式</h3>
+</br>
+<form action="user/fileupload2" method="post" enctype="multipart/form-data">
+    选择文件：<input type="file" name="upload"/>
+    <input type="submit" value="上传2"/>
+    </br>
+</form>
+
 </body>
 </html>
